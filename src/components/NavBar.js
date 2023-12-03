a196296bdf2cd2095baefbde127e88f62b1e93a9
Daniel Morley <morleydan10@gmail.com>
2023-12-02 21:30:18 -0500

    codes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" className="nav-link">Home</NavLink>
+      <NavLink to="/Actors" className="nav-link">Actors</NavLink>
+      <NavLink to="/Directors" className="nav-link">Directors</NavLink>
+      <NavLink to="/Movie:id" className="nav-link">Movie</NavLink>
     </nav>
     );
 };
